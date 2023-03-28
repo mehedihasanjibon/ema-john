faaf948c424bf3071703e05e9e25150674435dcf
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-29 05:27:04 +0600

    added load data and set state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="/order"> Order</a>
-                <a href="/Order-review"> Order Review</a>
-                <a href="/manage-inventory"> Manage Inventory</a>
-                <a href="/login"> Login</a>
+                <a href="/order"> Order </a>
+                <a href="/order-review"> Order Review </a>
+                <a href="/manage-inventory"> Manage Inventory </a>
+                <a href="/login"> Login </a>
             </div>
         </nav>
     );
