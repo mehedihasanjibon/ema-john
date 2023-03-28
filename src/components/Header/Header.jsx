21a53e7982a0227baa942cafdb1ad0dbab1b1315
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-29 03:06:57 +0600

    added the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/order"> Order</a>
+                <a href="/Order-review"> Order Review</a>
+                <a href="/manage-inventory"> Manage Inventory</a>
+                <a href="/login"> Login</a>
+            </div>
+        </nav>
     );
 };
 
