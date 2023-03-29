9fbc5b23dbb0e6d134d51c11a73ac6f3f531a0fc
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-29 17:00:56 +0600

    added product add , style debug and button position

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,13 +4,17 @@ import './Product.css';
 const Product = (props) => {
     // console.log(props);
     // console.log(props.product);
-    const {img, name, seller, quantity, price} = props.product;
+    const {img, name, seller, ratings, price} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <div>
-                <h6></h6>
+            <div className='product-info'>
+                <h6 className='product-name'> {name} </h6>
+                <p className='product-price'> Price: ${price}</p>
+                <p className='product-seller'> Manufacturer: ${seller}</p>
+                <p className='product-ratings'> Rating: {ratings} start</p>
             </div>
+            <button className='btn-cart'> Add to Cart </button>
         </div>
     );
 };
