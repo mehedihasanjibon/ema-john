c9a4cd31eccec17ee40e39af9ece70ac4521f7cc
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-29 16:33:03 +0600

    added the component layout, display data

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Product.css';
-const Product = () => {
+
+const Product = (props) => {
+    // console.log(props);
+    // console.log(props.product);
+    const {img, name, seller, quantity, price} = props.product;
     return (
-        <div>
-            
+        <div className='product'>
+            <img src={img} alt="" />
+            <div>
+                <h6></h6>
+            </div>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
-    useEffect (()=> {
+    useEffect( () =>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
@@ -13,7 +14,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                <h2> Products coming here: {products.length} </h2>
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product ={product}
+                    ></Product> )
+                }
             </div>
             <div className='cart-container'>
                 <h4> Order Summary </h4>
