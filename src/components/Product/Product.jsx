83617486cf0bc9715b577572b7be7dc1df4fe6c0
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-29 21:10:24 +0600

    added the cart state setup and react font awesome icon

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
-    // console.log(props);
+    console.log(props);
     // console.log(props.product);
     const {img, name, seller, ratings, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
+    
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -14,7 +20,10 @@ const Product = (props) => {
                 <p className='product-seller'> Manufacturer: ${seller}</p>
                 <p className='product-ratings'> Rating: {ratings} start</p>
             </div>
-            <button className='btn-cart'> Add to Cart </button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'> 
+            Add to Cart
+            <FontAwesomeIcon icon={faShoppingCart} />
+             </button>
         </div>
     );
 };
