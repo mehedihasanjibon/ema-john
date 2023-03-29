bb406fc9a1e194b4d1c477a90c7408ab22abce48
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-30 01:11:23 +0600

    added the create cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = () => {
+const Cart = ({cart}) => { //option-03
+    // const cart = props.cart; // option-01
+    // const {cart} = props; //option-02
+
+    console.log(cart);
+
+    let total = 0;
+    for(const product of cart){
+        total = total + product.price;
+    }
     return (
-        <div>
-            
+        <div className='cart'>
+            <h4> Order Summary </h4>
+            {/* <p> Selected Items: {cart.length}</p> */}
+            <p> Selected Items: {cart.length} </p>
+            <p> Total Price: ${total} </p>
+            <p> Total Shipping Charge: ${} </p>
+            <p> Tax: {}</p>
+            <h6> Grand Total: ${} </h6>
         </div>
     );
 };
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,7 +4,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props);
+    // console.log(props);
     // console.log(props.product);
     const {img, name, seller, ratings, price} = props.product;
     const handleAddToCart = props.handleAddToCart;
@@ -21,8 +21,8 @@ const Product = (props) => {
                 <p className='product-ratings'> Rating: {ratings} start</p>
             </div>
             <button onClick={() => handleAddToCart(props.product)} className='btn-cart'> 
-            Add to Cart
-            <FontAwesomeIcon icon={faShoppingCart} />
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
              </button>
         </div>
     );
